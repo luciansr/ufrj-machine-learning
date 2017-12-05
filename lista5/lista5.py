fd39c73694542c0f0acca5c0dbc0d259903eeade
Lucian Sturião Rodrigues <luciansr@gmail.com>
2017-12-05 01:59:52 -0200

    modificacoes

--- lista5/lista5.py
+++ lista5/lista5.py
@@ -37,7 +37,7 @@ def lista5():
 
     log_prob_obs = model.score(bob_says)
     print("log prob: " , log_prob_obs, " prob:", np.exp(log_prob_obs))
-    print("probabilities")
+    print("Prob of Temperatures")
     print(model.predict_proba(bob_says))
 
 def lista5_6():
@@ -69,6 +69,10 @@ def lista5_6():
     
 
 
+    maiorItem0 = [0,-1]
+    maiorItem1 = [0,-1]
+    maiorItem2 = [0,-1]
+    maiorItem3 = [0,-1]
 
     sequence = 1
     for item0 in range(2):
@@ -82,12 +86,96 @@ def lista5_6():
 
                     # alice_hears = model.predict(bob_says)
                     logprob, alice_hears = model.decode(bob_says, algorithm="viterbi")
-                    print ("Bob says:", ", ".join(map(lambda x: observations[x[0]], bob_says)))
-                    print ("Alice hears:", ", ".join(map(lambda x: states[x], alice_hears)))
+                    print ("Sizes:", ", ".join(map(lambda x: observations[x[0]], bob_says)))
+                    print ("Temperatures:", ", ".join(map(lambda x: states[x], alice_hears)))
 
                     log_prob_obs = model.score(bob_says)
-                    print("log prob: " , log_prob_obs, " prob:", np.exp(log_prob_obs))
+                    print("log prob of observations: " , log_prob_obs, " prob of observations:", np.exp(log_prob_obs))
                     print("probabilities")
-                    print(model.predict_proba(bob_says))
+                    probs = model.predict_proba(bob_says)
+                    print(probs)
 
-lista5_6()
+                    # 0
+                    if probs[0][0] > maiorItem0[0]:
+                        maiorItem0[0] = probs[0][0]
+                        maiorItem0[1] = 0
+
+                    if probs[0][1] > maiorItem0[0]:
+                        maiorItem0[0] = probs[0][1]
+                        maiorItem0[1] = 1
+
+                    # 1
+                    if probs[1][0] > maiorItem1[0]:
+                        maiorItem1[0] = probs[1][0]
+                        maiorItem1[1] = 0
+
+                    if probs[1][1] > maiorItem1[0]:
+                        maiorItem1[0] = probs[1][1]
+                        maiorItem1[1] = 1
+                    
+                    # 2
+                    if probs[2][0] > maiorItem2[0]:
+                        maiorItem2[0] = probs[2][0]
+                        maiorItem2[1] = 0
+
+                    if probs[2][1] > maiorItem2[0]:
+                        maiorItem2[0] = probs[2][1]
+                        maiorItem2[1] = 1
+
+                    # 3
+                    if probs[3][0] > maiorItem3[0]:
+                        maiorItem3[0] = probs[3][0]
+                        maiorItem3[1] = 0
+
+                    if probs[3][1] > maiorItem3[0]:
+                        maiorItem3[0] = probs[3][1]
+                        maiorItem3[1] = 1
+
+    print (maiorItem0)
+    print (maiorItem1)
+    print (maiorItem2)
+    print (maiorItem3)
+
+def lista5_6_3():
+
+    states = ["Hot", "Cold"]
+    n_states = len(states)
+
+    observations = ["Small", "Medium", "Large"]
+    n_observations = len(observations)
+
+    start_probability = np.array([0.5, 0.5])
+
+    transition_probability = np.array([
+        [0.75, 0.25],
+        [0.6, 0.4]
+    ])
+
+    emission_probability = np.array([
+        [0.05, 0.4, 0.55],
+        [0.8, 0.1, 0.1]
+    ])
+
+    model = hmm.MultinomialHMM(n_components=n_states, init_params="", n_iter=100, verbose=True)
+    model.startprob_ = start_probability
+    model.transmat_ = transition_probability
+    model.emissionprob_ = emission_probability
+
+    # predict a sequence of hidden states based on visible states
+    bob_says = np.array([[0,1,0,2]]).T
+
+    # alice_hears = model.predict(bob_says)
+    logprob, alice_hears = model.decode(bob_says, algorithm="viterbi")
+    print ("Sizes:", ", ".join(map(lambda x: observations[x[0]], bob_says)))
+    print ("Temperatures:", ", ".join(map(lambda x: states[x], alice_hears)))
+
+    log_prob_obs = model.score(bob_says)
+    print("log prob of observations: " , log_prob_obs, " prob of observations:", np.exp(log_prob_obs))
+    print("probabilities")
+    probs = model.predict_proba(bob_says)
+    print(probs)
+
+                
+                  
+
+lista5_6_3()
